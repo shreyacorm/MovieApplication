d8eba966b20a87f57da68a2b39270c3e5b6dca1b
shreya shankar <shreyashankar254@gmail.com>
2019-12-19 21:24:36 +0530

    First commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Index1()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcomes(string name, int Id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Id is: {Id}");
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
